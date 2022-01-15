323b4f168e57df27ef129ea2f25f59faff5a7c7b
Hongchao Liu <lbs.lhc@gmail.com>
2022-01-15 10:19:52 +0100

    Parameterize on different chains

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,20 +1,30 @@
-from brownie import FundMe, network, MockV3Aggregator
-from scripts.helpful_scripts import get_account
+from brownie import FundMe, network, MockV3Aggregator, config
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mock,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
+
 
 def deploy_fund_me():
     account = get_account()
     # pass the price feed
 
-    if network.show_active() != "development":
-        price_feed_address = config["networks"][network.show_active()]["eth_usd_price_feed"]
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
     else:
-        print(f"The active network is {network.show_active()}")
-        print("Deploying mocks")
-        mock_aggregator = MockV3Aggregator.deploy(18, 200000000000000000000, {"from", account})
-        price_feed_address = mock_aggregator.address
-        print("Mock Deployed")
+        deploy_mock()
+        price_feed_address = MockV3Aggregator[
+            -1
+        ].address  # latest deployed MockV3Aggregator
 
-    fund_me = FundMe.deploy(price_feed_address, {"from": account}, publish_source=True)
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()]["verify"],
+    )
     print(f"Contract deployed to {fund_me.address}")
 
 
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,7 +1,22 @@
-from brownie import accounts, config, network
+from brownie import accounts, config, network, MockV3Aggregator
+from web3 import Web3
+
 
 def get_account():
-    if network.show_active() == "development":
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
+
+
+def deploy_mock():
+    print(f"The active network is {network.show_active()}")
+    print("Deploying mocks")
+    if len(MockV3Aggregator) <= 0:
+        mock_aggregator = MockV3Aggregator.deploy(
+            18, Web3.toWei(2000, "ether"), {"from": get_account()}
+        )
+    print("Mock Deployed")
+
+
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]

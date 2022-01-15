3c89d6230acf3baa873c5a51c7a3bdbfd8fb501d
Hongchao Liu <lbs.lhc@gmail.com>
2022-01-15 12:12:21 +0100

    Test using mainnet fork

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -3,7 +3,10 @@ from web3 import Web3
 
 
 def get_account():
-    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
@@ -20,3 +23,4 @@ def deploy_mock():
 
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
